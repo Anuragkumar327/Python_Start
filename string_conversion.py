7729bc105f58c192e8979f3e046614ebbd3c4a58
Anurag Kumar <januragkumar327@gmail.com>
2026-03-31 21:30:40 +0530

    Add string conversion script

---
+++ string_conversion.py
@@ -0,0 +1,34 @@
+
+
+
+# Printing the indexing in the code
+
+a = "Hello this is Anurag"
+print(a[3])  # l is printed in this 
+
+
+
+
+# For Printing the Negative Indexing in the Python 
+b = " Hello EveryOne"
+print(b[-2]) 
+
+# in this letter is printed from Back side
+
+
+
+# String Slicing in Python 
+
+# string[start : stop : step]
+
+text = "Hello World !"
+
+print(text[0:5]) # Hello
+
+print(text[:5]) # Hello (start from the beginning)
+
+print(text[7:]) # world (go to end)
+ 
+print(text[::-1])  # DLROWOLLEH  Reverse is printed in this 
+
+# data Type

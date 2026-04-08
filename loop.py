8be341470cd942cad3bbe694a06c2f165014daf9
Anurag Kumar Jha <januragkumar327@gmail.com>
2026-04-09 01:35:29 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,28 @@
+
+
+# Loop in the Python
+
+for i in range(5):
+    print(i)
+
+
+
+#*************************************
+
+fruits = ["Apple","Banana","Mango"]
+for f in  fruits:
+    print(f)
+
+
+
+student = {"name":"Anurag","Age":23}
+for key in student:
+    print(key,"=",student[key])
+
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+# Here 5 is skipped in this 
